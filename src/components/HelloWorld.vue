8ad9acec7a8289b018aedc4e3fcd8e8703fd4d1c
Damis Garcia <damisgarcia@gmail.com>
2018-11-19 17:40:34 -0300

    Kick stater

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,65 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container fill-height>
+    <v-layout
+      text-xs-center
+      fill-height
+      justify-center
+      wrap
+    >      
+    <v-flex mb-12>
+      <v-form v-model="valid">
+        <v-container grid-list-lg>
+          <div class="mt-5 mb-4">
+            <div class="display-3">Send2WPP</div>
+            <p>A maneira mais fácil de conversar no whatsapp sem adicionar contato</p>
+          </div>
+          <v-layout row wrap justify-center>
+            <v-flex xs2>
+              <v-text-field
+                v-model="countryCode"
+                name=""
+                label="Country Code"
+                :rules="countryCodeRules"
+                required
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs8>
+              <v-text-field                  
+                v-model="phoneNumber"
+                label="Phone Number"
+                :rules="phoneNumberRules"
+                required
+              ></v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row justify-center>
+            <v-flex xs3>
+              <v-btn color="primary" :disabled="!valid" :href="`https://api.whatsapp.com/send?phone=${countryCode}${phoneNumber}&text=Olá`" target="blank" :large="true" :block="true" :round="true">Enviar</v-btn>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-form>
+    </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    data: () => ({
+      valid: false,
+      countryCode: '',
+      countryCodeRules: [
+        v => !!v || 'Country Code is required'
+      ],
+      phoneNumber: '',
+      phoneNumberRules: [
+        v => !!v || 'Phone Number is required'
+      ]
+    })
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
